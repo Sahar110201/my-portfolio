5c3b6b8160e83d8b79cf218f59169ac4993f3827
Sahar110201 <saharadeeb7@gmail.com>
2025-11-04 17:53:51 -0500

    Update contact form and JS

---
+++ script.js
@@ -0,0 +1,53 @@
+const buttons = document.querySelectorAll('.detail-btn');
+buttons.forEach(button => {
+    button.addEventListener('click', () => { 
+    const details = button.nextElementSibling;
+
+    if (details.style.display === 'block') {
+        details.style.display = 'none';
+        button.textContent = 'Show Detail';
+    } else {
+        details.style.display = 'block';
+        button.textContent = 'Hide Detail';
+    }
+});
+});
+
+
+function validateEmail(emailaddress) {
+    const regex = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/;
+    return regex.test(emailaddress);
+
+}
+//form validation
+function formValidation() {
+    const form = document.getElementById("contactForm"); 
+
+    const name = document.getElementById("name");
+    const emailAddress = document.getElementById("emailaddress");
+    const message = document.getElementById("message");
+
+    const errorName = document.getElementById("nameError");
+    const errorEmail = document.getElementById("emailError")
+    const errorMessage = document.getElementById("messageError");
+
+form.addEventListener("submit", (event) => {
+    errorName.textContent = " ";
+
+    if (name.value.trim() == "") {
+        console.log(errorName);
+
+        errorName.textContent = "Please enter your full name";
+        event.preventDefault();
+    }
+    if (emailAddress.value.trim() == "") {
+        errorEmail.textContent = "Please enter your email address";
+        event.preventDefault();
+    }
+    if (message.value.trim() == "") {
+        errorMessage.textContent = "Please enter a message";
+        event.preventDefault();
+    }
+})
+}
+
