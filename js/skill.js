1b48b4ce61bf83dc78dbd709aa653ab9d6f24452
Sahar110201 <saharadeeb7@gmail.com>
2025-12-07 01:49:03 -0500

    Update all files

--- js/skill.js
+++ js/skill.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
         skill.style.width = "0%";
 
         if (skill.classList.contains("html")) {
-            skill.style.width = "90%";
+            skill.style.width = "100%";
         } else if (skill.classList.contains("css")) {
             skill.style.width = "90%";
         } else if (skill.classList.contains("js")) {
             skill.style.width = "60%";
         } else if (skill.classList.contains("react")) {
-            skill.style.width = "0%"; 
+            skill.style.width = "1%"; 
         } else if (skill.classList.contains("git")) {
             skill.style.width = "55%";
         }
