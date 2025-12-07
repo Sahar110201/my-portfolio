593b07f4c4c091357356c4e3cdd5cec6991948ec
Sahar110201 <saharadeeb7@gmail.com>
2025-12-07 02:42:53 -0500

    Update detailbtn.js

--- js/detailbtn.js
+++ js/detailbtn.js
@@ -1,14 +1,15 @@
 const buttons = document.querySelectorAll('.detail-btn');
-buttons.forEach(button => {
-    button.addEventListener('click', () => { 
-    const details = button.nextElementSibling;
 
-    if (details.style.display === 'block') {
-        details.style.display = 'none';
-        button.textContent = 'Show Detail';
-    } else {
-        details.style.display = 'block';
-        button.textContent = 'Hide Detail';
-    }
-});
-});
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        const details = buttons[i].nextElementSibling;
+
+        if (details.style.display === 'block') {
+            details.style.display = 'none';
+            buttons[i].textContent = 'Show Detail';
+        } else {
+            details.style.display = 'block';
+            buttons[i].textContent = 'Hide Detail';
+        }
+    });
+}
