33ad6de0deb224aac1056584d92ad9c2b9b28a8a
Sahar110201 <saharadeeb7@gmail.com>
2025-12-06 21:17:48 -0500

    Update skill.js and style.css

--- js/skill.js
+++ js/skill.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const skillLevels = document.querySelectorAll(".skill-level");
+    for (let i = 0; i < skillLevels.length; i++) {
+        let skill = skillLevels[i];
+        let width = "0%"; 
 
-    skillLevels.forEach(skill => {
-        let width;
-        if (skill.classList.contains("html")) width = "90%";
-        else if (skill.classList.contains("css")) width = "85%";
-        else if (skill.classList.contains("js")) width = "80%";
-        else if (skill.classList.contains("react")) width = "70%";
-        else if (skill.classList.contains("git")) width = "75%";
-
-        setTimeout(() => {
+        if (skill.classList.contains("html")) {
+            width = "100%";
+        } else if (skill.classList.contains("css")) {
+            width = "90%";
+        } else if (skill.classList.contains("js")) {
+            width = "60%";
+        } else if (skill.classList.contains("react")) {
+            width = "0%";
+        } else if (skill.classList.contains("git")) {
+            width = "55%";
+        }
             skill.style.width = width;
-        }, 300);
-    });
+    }
 });
